85cc9eb5892f9c79572f52e53d41fa82970d2fc8
Sakal <samnang.sakal@gmail.com>
2023-03-08 16:05:51 +0700

    fix the withdraw, use withdrawn TempAmount

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ function App() {
   const [address, updateAddress] = useState("");
 
   //const [walletAddress, updateWalletAddress] = useState("");
-  const [balance, updateBalance] = useState(0);
+  const [balance, updateBalance] = useState("");
+  const [withDrawalTime, updateWithDrawalTime] = useState("");
 
   // Define a function to fetch the balance of the signer's wallet address
   const getWalletBalance = async () => {
@@ -37,44 +38,56 @@ function App() {
     const contract = new ethers.Contract(ABI.address, ABI.abi, signer);
     const stake = await contract.stake({
       value: ethers.utils.parseEther("0.001"),
+      gasLimit: 150000
     });
+    //listeing to event of Staking
+    await contract.on("Staked", (account, amount, timestamp) => {
+      console.log(`Account ${account} staked ${amount} at timestamp ${timestamp}`);
+    });
+    //withDrawalTimeLeft();
     return stake;
   }
 
   async function Withdraw() {
     const contract = new ethers.Contract(ABI.address, ABI.abi, signer);
 
-    const withdraw = await contract.withdraw();
+    const withdraw = await contract.withdraw({gasLimit: 150000});
+
+    await contract.on("Withdrawn", (account, amount, timestamp) => {
+        console.log(`Withdrawn Event: \nAccount: ${account} \nAmount: ${amount} \nTimestamp: ${timestamp}`);
+    })
     return withdraw;
   }
 
-  async function Execute() {
-    const contract = new ethers.Contract(ABI.address, ABI.abi, signer);
+  // async function Execute() {
+  //   const contract = new ethers.Contract(ABI.address, ABI.abi, signer);
 
-    const execute = await contract.execute({
-      gasLimit: 500000, // Set the gas limit to 500,000 (replace with your desired value)
-    });
-    return execute;
-  }
+  //   const execute = await contract.execute({
+  //     gasLimit: 500000, // Set the gas limit to 500,000 (replace with your desired value)
+  //   });
+  //   return execute;
+  // }
 
   async function Balance() {
     const contract = new ethers.Contract(ABI.address, ABI.abi, signer);
     // Get the balance of the current user
     const currentUserAddress = await signer.getAddress();
-    const balance = await contract.balances(currentUserAddress);
-    const readableBalance = new BigNumber(balance)
-      .dividedBy(10 ** 18)
-      .toFixed(2);
+    const balance = await contract.balanceOf(currentUserAddress);
+    // const readableBalance = new BigNumber(balance)
+    //   .dividedBy(10 ** 18)
+    //   .toFixed(2);
     //console.log(readableBalance.fixed(18)); // show the balance
-    return readableBalance;
+    updateBalance(balance.toString());
+    return balance;
   }
 
-  async function withDrawalTimeLeft() {
-    const contract = new ethers.Contract(ABI.address, ABI.abi, signer);
-    // Check the time remaining before the minimum staking period has passed
-    const timeRemaining = await contract.withdrawalTimeLeft();
-    console.log(`Time remaining: ${timeRemaining} seconds`);
-  }
+  // async function withDrawalTimeLeft() {
+  //   const contract = new ethers.Contract(ABI.address, ABI.abi, signer);
+  //   // Check the time remaining before the minimum staking period has passed
+  //   const timeRemaining = await contract.withdrawalTimeLeft();
+  //   console.log(`Time remaining: ${timeRemaining} seconds`);
+  //   updateWithDrawalTime(timeRemaining);
+  // }
 
   // async function emitEvent() {
   //   const contract = new ethers.Contract(ABI.address, ABI.abi, signer);
@@ -98,6 +111,7 @@ function App() {
     // rewardRatePerSecond();
     console.log("Contract Address: ", ABI.address);
     console.log("ABI CONTRACT: ", ABI.abi);
+    console.log("Balance: ", balance)
   }, []);
 
   return (
@@ -107,6 +121,7 @@ function App() {
 
         <p>User Address: {address}</p>
         <p>User Balance: {balance}</p>
+        {/* <h3> WithDrawal Time: {withDrawalTime}</h3> */}
         <p>Please click here to stake</p>
         <button className="btn" onClick={Stake}>
           Stake 0.001 ether
@@ -114,18 +129,11 @@ function App() {
         <button className="btn" onClick={Withdraw}>
           Withdraw
         </button>
-        <button className="btn" onClick={Execute}>
+        {/* <button className="btn" onClick={Execute}>
           Execute
-        </button>
+        </button> */}
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
